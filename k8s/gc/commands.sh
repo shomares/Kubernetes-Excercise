c794e36d72eb9664d73bdebd5c96aa337be8ac17
eneas <ebaltazar@bside.com.mx>
2022-03-28 16:56:13 -0600

    Encrypted file

--- k8s/gc/commands.sh
+++ k8s/gc/commands.sh
@@ -6,3 +6,6 @@ helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.git
 kubectl create ingress demo-localhost --class=nginx --rule=demo.localdev.me/*=demo:80
 
 kubectl set image Deployment/server-deployment server=devtodocontainer.azurecr.io/multi-server:v1
+
+
+docker run -it -v C:/Users/ebaltazar/code\practicas/kubernetes/complex/k8s:/app ruby:2.4 sh
